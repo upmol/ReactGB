0460f683f72b1f0458f9318083527b44ea50de56
MolyaKsu <ksushamolya@icloud.com>
2022-10-12 11:17:10 +0300

    React lisson1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Message } from "./components/Message";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Message textMassage={"ПРИВЕТ Я REACT! КАК ДЕЛА"} />
+    </div>
+  );
+};
